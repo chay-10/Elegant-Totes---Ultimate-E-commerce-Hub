dd4b6b05f79556145deb873648bf199574b15567
chay-10 <125801269+chay-10@users.noreply.github.com>
2024-09-22 20:53:25 +0530

    Add files via upload

---
+++ inc/languages/ar.php
@@ -0,0 +1,16 @@
+<?php
+$lang = [
+  'Elegant Totes' => 'سعيد لكويط',
+  'Ltr' => 'rtl',
+  'En' => 'en',
+  'Bootstrap' => 'bootstrap.rtl.min.css',
+  'English' => 'انجليزي',
+  'Arabic' => 'عربي',
+  'Shop' => 'متجر',
+  'FAQ' => 'أسئلة شائعة',
+  'Tracking' => 'تتبع',
+  'Contact Us' => 'اتصل بنا',
+  'Search' => 'بحث',
+  'Elegant Totes Store' => 'متجر سعيد لكويط',
+  'Last Products' => 'آخر المنتجات'
+];
---
+++ inc/languages/en.php
@@ -0,0 +1,17 @@
+<?php
+$lang = [
+  'Elegant Totes' => 'Elegant Totes',
+  'Ltr' => 'Ltr',
+  'En' => 'en',
+  'Bootstrap' => 'bootstrap.min.css',
+  'English' => 'English',
+  'Arabic' => 'Arabic',
+  'Shop' => 'Shop',
+  'FAQ' => 'FAQ',
+  'Tracking' => 'Tracking',
+  'Contact Us' => 'Contact Us',
+  'Search' => 'Search',
+  'Elegant Totes Store' => 'Elegant Totes Store',
+  'Last Products' => 'Last Products',
+  '' => ''
+];
